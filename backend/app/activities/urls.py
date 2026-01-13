a1dd7bf7d843c197bce5288675154cfc1c9998d1
Mafin33nut <timofeiigonkin@yandex.ru>
2026-01-13 21:57:18 +0300

    2st api change

--- backend/app/activities/urls.py
+++
@@ -1,4 +0,0 @@
-from django.urls import path, include from rest_framework.routers import DefaultRouter from .views import ( ActivityCategoryViewSet, ActivityViewSet, ActivityLogViewSet, ActivityRewardViewSet, ActivityTimerViewSet )
-router = DefaultRouter() router.register(r'categories', ActivityCategoryViewSet, basename='activity-category') router.register(r'activities', ActivityViewSet, basename='activity') router.register(r'logs', ActivityLogViewSet, basename='activity-log') router.register(r'rewards', ActivityRewardViewSet, basename='activity-reward') router.register(r'timers', ActivityTimerViewSet, basename='activity-timer')
-urlpatterns = [ path('', include(router.urls)), ]
-________________________________________
