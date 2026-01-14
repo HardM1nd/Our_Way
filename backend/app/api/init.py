f76015e97deaeb8c2bd27fe791e82a1ed201850f
MatveyMolotov <accwwwaaaa@gmail.com>
2026-01-14 17:55:55 +0300

    3rd api change

--- backend/app/api/init.py
+++ backend/app/api/init.py
@@ -1,2 +1,7 @@
+<<<<<<< HEAD
 """ Central API package. Exports a router which aggregates sub-app routers. """ from .urls import router # noqa: F401
+=======
+""" Central API package. Exports a router which aggregates sub-app routers. """ 
+from .urls import router # noqa: F401
+>>>>>>> a9cc341 (3rd api change)
 
