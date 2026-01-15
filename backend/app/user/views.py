de28d512eea27a8d2b842da8fee94016455e1b5b
Your Name <you@example.com>
2026-01-15 19:32:00 +0300

    users mod 1

--- backend/app/user/views.py
+++ backend/app/user/views.py
@@ -1,8 +1,7 @@
-from rest_framework import viewsets, permissions
-from .models import User
+from rest_framework import viewsets, permissions 
+from .models import User 
 from .serializers import UserSerializer
-
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+class UserViewSet(viewsets.ModelViewSet): 
+    queryset = User.objects.all() 
+    serializer_class = UserSerializer 
     permission_classes = [permissions.IsAuthenticated]
