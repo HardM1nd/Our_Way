fa4ead6e033cc30fa0c6585f1aba23bd166447fa
Your Name <you@example.com>
2026-01-17 14:46:56 +0300

    small mods backend

--- backend/app/apps.py
+++ backend/app/apps.py
@@ -1,2 +1,4 @@
 from django.apps import AppConfig
-class AppConfigDefault(AppConfig): name = 'app' verbose_name = 'Main App'
+class AppConfigDefault(AppConfig): 
+    name = 'app' 
+    verbose_name = 'Main App'
